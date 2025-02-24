91f2853ebf470446a48232aa0dcd422fd18b02a7
cuhs <alexhu00374@gmail.com>
2025-02-23 22:07:46 -0800

    playing around with textures and design

--- engineering-portfolio/src/app/page.tsx
+++ engineering-portfolio/src/app/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { Canvas, useFrame } from "@react-three/fiber";
+import { Canvas, useFrame, useLoader } from "@react-three/fiber";
 import { OrbitControls } from "@react-three/drei";
 import { motion } from "framer-motion";
 import "tailwindcss/tailwind.css";
@@ -10,6 +10,9 @@ import * as THREE from "three";
 function Planet({ radius, speed, color, href, initialAngle }: { radius: number; speed: number; color: string; href: string; initialAngle: number }) {
   const [zoom, setZoom] = useState(false);
   const planetRef = useRef<THREE.Mesh>(null);
+
+  const marsTexture = useLoader(THREE.TextureLoader, "/mars-mode.jpg");
+
 
   useFrame(({ clock }) => {
     if (planetRef.current) {
@@ -29,20 +32,21 @@ function Planet({ radius, speed, color, href, initialAngle }: { radius: number; 
       scale={zoom ? [3, 3, 3] : [2.5, 2.5, 2.5]}
     >
       <sphereGeometry args={[1, 32, 32]} />
-      <meshStandardMaterial color={color} metalness={0.3} roughness={0.5} />
+      <meshStandardMaterial map={marsTexture} metalness={0.3} roughness={0.5} />
     </mesh>
   );
 }
 
 export default function WelcomePage() {
+  const sunTexture = useLoader(THREE.TextureLoader, "/sun-mode.jpg")
   return (
     <motion.div className="bg-black text-white w-screen h-screen fixed top-0 left-0">
       <Canvas camera={{ position: [0, 0, 30] }} className="w-full h-full">
         <mesh>
           <sphereGeometry args={[5, 32, 32]} />
-          <meshStandardMaterial color="yellow" emissive="yellow" emissiveIntensity={1} />
+          <meshStandardMaterial map = {sunTexture} emissiveMap={sunTexture} emissive="white" emissiveIntensity={2} />
         </mesh>
-        <pointLight position={[0, 0, 0]} intensity={1000} />
+        <pointLight position={[0, 0, 0]} intensity={1000} /> 
         <Planet radius={10} speed={0.4} color="blue" href="/individual" initialAngle={Math.PI / 3} />  
         <Planet radius={18} speed={0.2} color="red" href="/team" initialAngle={Math.PI / 6} />  
         <OrbitControls minDistance={10} maxDistance={30} enableDamping={true} dampingFactor={0.1} />
